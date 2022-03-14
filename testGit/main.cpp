27c0c7f3ee6aad13e25f0fc713ef37d055b97acf
felones <vladikkadola@gmail.com>
2022-03-15 00:57:21 +0300

    bugfix

--- testGit/main.cpp
+++ testGit/main.cpp
@@ -8,11 +8,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     cout << "hello, Vlad\n";
-<<<<<<< HEAD
     cout << "this is branch develop\n";
-=======
-    cout << "this is develop2\n";
->>>>>>> develop2
 
     return 0;
 }
